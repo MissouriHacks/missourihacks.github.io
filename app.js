618da69338ad7aaf20d07921db28f46c965ae91b
Derek Nissen <dereknissen23@gmail.com>
2023-12-19 20:35:32 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+/* FAQ ACCORDIAN */
+var acc = document.getElementsByClassName("faq-link");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var desc = this.nextElementSibling; /* gets the next element, aka the description component */
+    if (desc.style.maxHeight == desc.scrollHeight + "px") {
+       /* close down */
+        desc.style.maxHeight = "0px";
+    } else {
+        /* open up */
+        desc.style.maxHeight = desc.scrollHeight + "px";
+    }
+  });
+}
